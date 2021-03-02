921d2448bfb18802e68c58345e088a73ea11c8ce
Fabian Riewe <fabianriewe00@gmail.com>
2021-03-02 16:12:18 +0100

    feat: added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as req:
+    requires = req.readlines()
+
+setuptools.setup(
+    name="limestone",
+    version="0.0.0",
+    author="Fabian Riewe",
+    author_email="fabian@kyve.network",
+    description="A python wrapper for limestone.finance",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fabianriewe/limestone-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requires
+)
